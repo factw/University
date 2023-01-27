f1ca3db3ed3efb15ff7d39d9ba01f6cbe0f387e2
FC <coria.facundo.ariel@outlook.com>
2023-01-26 23:50:27 -0300

    Relaciones

---
+++ UniversityApiBackend/Models/DataModels/Course.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Course : BaseEntity
+    {
+        [StringLength(50)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [StringLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+        
+        [Required]
+        public string Description{ get; set; } = string.Empty;
+
+        public Level Level { get; set; } = Level.Basic;
+
+        [Required]
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+
+        [Required]
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+
+        [Required]
+        public Chapter Chapter { get; set; } = new Chapter();
+    }
+
+    public enum Level 
+    { 
+        Basic,
+        Medium, 
+        Advanced,
+        Expert
+    }
+}
